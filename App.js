5e3bd1610d8329cb02922c67ad67e9b274dbbac3
단단한슬라임 <66393239+js-slime0614@users.noreply.github.com>
2022-09-15 20:05:41 +0900

    weather app bones

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flexDirection: "row" }}>
-      <View style={{width:200, height:200, backgroundColor:"tomato"}}></View>
-      <View style={{width:200, height:200, backgroundColor:"teal"}}></View>
-      <View style={{width:200, height:200, backgroundColor:"orange"}}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container : {flex:1, backgroundColor:"tomato"},
+  city : {
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  cityName: {
+    color:"white",
+    fontSize:48,
+    fontWeight: "500",
+  },
+  weather:{
+    flex:3,
+  },
+  day: {
+    backgroundColor: "teal",
+    flex:1,
+    alignItems:"center",
+  },
+  temp: {
+    marginTop:50,
+    fontSize:170,
+  },
+  description:{
+    fontSize: 60,
+    marginTop: -30,
+  },
+})
