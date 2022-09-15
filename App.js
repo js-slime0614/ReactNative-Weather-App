267d2890aff5ee0aa22494e00b50b267c6a84dd5
단단한슬라임 <66393239+js-slime0614@users.noreply.github.com>
2022-09-15 15:51:48 +0900

    why

--- App.js
+++ App.js
@@ -1,23 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello! I made a RN app</Text>
-      <StatusBar style="auto" />
+    <View style={{ flexDirection: "row" }}>
+      <View style={{width:200, height:200, backgroundColor:"tomato"}}></View>
+      <View style={{width:200, height:200, backgroundColor:"teal"}}></View>
+      <View style={{width:200, height:200, backgroundColor:"orange"}}></View>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize:28,
-  }
-});
